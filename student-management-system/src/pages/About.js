b7de7306f7488f1a64a7f04b87e4c638ad440ad3
Ragi-Varshini-Reddy <varshiniragi1807@gmail.com>
2011-05-21 15:25:21 +0530

    Added Student Management System React App

---
+++ student-management-system/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PageLayout.css';
+
+const About = () => (
+  <div className="page">
+    <h2>About</h2>
+    <p>This is the About section routed page of Student Management System.</p>
+  </div>
+);
+
+export default About;
